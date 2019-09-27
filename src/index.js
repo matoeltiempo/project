330feebabc670e5e3035272ae1c041c28c4baf19
matoeltiempo <matoeltiempoav@gmail.com>
2019-09-27 18:20:45 +0300

    module API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './index.css';
 
 import CardList from './js/cardlist.js';
+import Api from './js/Api';
+
 import {profileInfo} from './js/profileinfo.js';
 import {saveAvatar} from './js/saveavatar.js';
 import {activateError, resetError} from './js/activateError';
+
 
 const root = document.querySelector('.root');
 const placesList = root.querySelector('.places-list');
@@ -127,62 +130,7 @@ export const popupEditLvl = new Popup(editUserLevel);
 export const popupImageLvl = new Popup(imageLevel);
 export const popupAvatarLvl = new Popup(avatarLevel);
 
-export class Api {
-    constructor(options) {
-        this.baseUrl = options.baseUrl;
-        this.headers = options.headers;
-    }
-    getResponseJson(result) {
-        if (result.ok) {
-            console.log(result);
-            return Promise.resolve(result.json());
-        }
-        return Promise.reject(result.status);
-    }
-    async getUserData() {
-        const result = await fetch(`${this.baseUrl}/users/me`, { headers: this.headers })
-        return this.getResponseJson(result);
-    }
-    async getInitialCards() {
-        const result = await fetch(`${this.baseUrl}/cards`, { headers: this.headers })
-        return this.getResponseJson(result);
-    }
-    async setUserData(name, info) {
-        const result = await fetch(`${this.baseUrl}/users/me`, {
-            method: 'PATCH', headers: this.headers,
-            body: JSON.stringify({
-                name: `${name}`,
-                about: `${info}`
-            })
-        })
-        return this.getResponseJson(result);
-    }
-    async setAddCard(name, link) {
-        const result = await fetch(`${this.baseUrl}/cards`, {
-            method: 'POST', headers: this.headers,
-            body: JSON.stringify({
-                name: `${name}`,
-                link: `${link}`
-            })
-        })
-        return this.getResponseJson(result);
-    }
-    async setUserAvatar(url) {
-        const result = await fetch(`${this.baseUrl}/users/me/avatar`, {
-            method: 'PATCH', headers: this.headers,
-            body: JSON.stringify({
-                avatar: `${url}`
-            })
-        })
-        return this.getResponseJson(result);
-    }
-    async deleteCard(id) {
-        const result = await fetch(`${this.baseUrl}/cards/${id}`, {
-            method: 'DELETE', headers: this.headers
-        })
-        return this.getResponseJson(result);
-    }
-}
+
 
 export const api = new Api(userOptions);
 
---
+++ src/js/Api.js
@@ -0,0 +1,56 @@
+export default class Api {
+    constructor(options) {
+        this.baseUrl = options.baseUrl;
+        this.headers = options.headers;
+    }
+    getResponseJson(result) {
+        if (result.ok) {
+            console.log(result);
+            return Promise.resolve(result.json());
+        }
+        return Promise.reject(result.status);
+    }
+    async getUserData() {
+        const result = await fetch(`${this.baseUrl}/users/me`, { headers: this.headers })
+        return this.getResponseJson(result);
+    }
+    async getInitialCards() {
+        const result = await fetch(`${this.baseUrl}/cards`, { headers: this.headers })
+        return this.getResponseJson(result);
+    }
+    async setUserData(name, info) {
+        const result = await fetch(`${this.baseUrl}/users/me`, {
+            method: 'PATCH', headers: this.headers,
+            body: JSON.stringify({
+                name: `${name}`,
+                about: `${info}`
+            })
+        })
+        return this.getResponseJson(result);
+    }
+    async setAddCard(name, link) {
+        const result = await fetch(`${this.baseUrl}/cards`, {
+            method: 'POST', headers: this.headers,
+            body: JSON.stringify({
+                name: `${name}`,
+                link: `${link}`
+            })
+        })
+        return this.getResponseJson(result);
+    }
+    async setUserAvatar(url) {
+        const result = await fetch(`${this.baseUrl}/users/me/avatar`, {
+            method: 'PATCH', headers: this.headers,
+            body: JSON.stringify({
+                avatar: `${url}`
+            })
+        })
+        return this.getResponseJson(result);
+    }
+    async deleteCard(id) {
+        const result = await fetch(`${this.baseUrl}/cards/${id}`, {
+            method: 'DELETE', headers: this.headers
+        })
+        return this.getResponseJson(result);
+    }
+}
