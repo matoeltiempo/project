c82d57a74551ef4bb67bd520927890c339cf8a66
matoeltiempo <matoeltiempoav@gmail.com>
2019-09-27 14:54:33 +0300

    copy html

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             inject: false,
             hash: true,
-            template: './index.html',
+            template: './src/index.html',
             filename: 'index.html'
         }),
         new WebpackMd5Hash(),
