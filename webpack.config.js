74dfc49555fb0a10322773e936d6cd215cfef79c
matoeltiempo <matoeltiempoav@gmail.com>
2019-09-28 16:44:55 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: '[name].[chunkhash].js'
     },
     module: {
